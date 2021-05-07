108aac9a61b19332d9c9dd816e69a8813b547422
StepanG <gurov.sd@gmail.com>
2021-05-07 21:04:36 +0300

    сделано задание 8.2

--- Урок 8/2 подгрузка постов/код/load.js
+++ Урок 8/2 подгрузка постов/код/load.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // 1. Получите ссылку на .scrollCheck
-let scrollCheck = ;
+let scrollCheck = document.querySelector('.scrollCheck');
 
 /**
  * Инициализация. При открытии страницы вставляем несколько постов
@@ -10,8 +10,12 @@ let scrollCheck = ;
 function init() {
     // 4. Если условие истинно, то вызывайте insertPosts и рекурсивно вызывайте
     // init.
-    if (window.innerHeight === document.body.offsetHeight) {
-        
+
+    // почему-то offsetHight у body при загрузке всегда на 1 пиксель больше.
+    //Добавил вычитание этого пикселя в проверку условия, но кажется этот способ не очень надежным
+    if (window.innerHeight === document.body.offsetHeight - 1) {
+        insertPosts();
+        init();
     }
 }
 init();
@@ -19,7 +23,7 @@ init();
 document.addEventListener('scroll', function(event) {
     if (scrollCheck.getBoundingClientRect().top <= window.innerHeight) {
         // 5. Если условие истинно вызывайте insertPosts.
-        
+        insertPosts();
     }
 });
 
@@ -31,9 +35,11 @@ function insertPosts() {
     // разметку, возвращаемую getPostMarkup.
     // 3.1 в getPostMarkup в качестве аргумента передавайте счетчик цикла
     let postsMarkup = '';
-    
+    for(let i = 0; i < 2; i++){
+        postsMarkup += getPostMarkup(i);
+    }
     // 3.2 Перед scrollCheck вставьте записанную в postsMarkup разметку
-    
+    scrollCheck.insertAdjacentHTML('beforebegin', postsMarkup);
 }
 
 /**
